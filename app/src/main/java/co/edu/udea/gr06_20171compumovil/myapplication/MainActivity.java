69836e6a0b3b7111856df8639b42af7d66b086db
james garzon <james@experimentality.co>
2017-04-18 17:32:36 -0500

    Firt commit

---
+++ app/src/main/java/co/edu/udea/gr06_20171compumovil/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package co.edu.udea.gr06_20171compumovil.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
